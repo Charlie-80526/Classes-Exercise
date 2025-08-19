1042c2a6876166c299e92a859deada7adbb1db92
Luis Yanez <lyanez0210@hotmail.com>
2025-08-19 11:00:07 -0500

    initial commit

--- Classes/Program.cs
+++ Classes/Program.cs
@@ -4,7 +4,12 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var carOne = new Car();
+            carOne.Make = "Ford";
+            carOne.Model = "Explorer";
+            carOne.Year = 2025;
+
+            Console.WriteLine($"This is a {carOne.Make} {carOne.Model}, made in the year {carOne.Year}");
         }
     }
 }
